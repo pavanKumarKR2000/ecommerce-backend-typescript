fdca6579f6e4da31929e1f507f86639641b8e64d
Pavankumar K R <pavan123456pop@gmail.com>
2025-06-14 18:35:43 +0530

    minor add in drizzle.config.ts file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: ["./src/db/user.schema.ts", "./src/db/product.schema.ts"],
+  schema: [
+    "./src/db/user.schema.ts",
+    "./src/db/product.schema.ts",
+    "./src/db/cart.schema.ts",
+    "./src/db/cartItem.schema.ts",
+  ],
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
