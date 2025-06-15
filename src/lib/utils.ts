ce007c024059cc2326e2c08c7bfc0618ce042824
Pavankumar K R <pavan123456pop@gmail.com>
2025-06-15 17:01:23 +0530

    add utility file

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+import { Request } from "express";
+
+export const getToken = (req: Request) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  return token;
+};
