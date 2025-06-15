512ddbb3e758bc657f46410969dc8e3e7ff15990
Pavankumar K R <pavan123456pop@gmail.com>
2025-06-15 17:02:08 +0530

    use a utility function getToken

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -98,6 +98,8 @@ export const signInController = async (req: Request, res: Response) => {
   }
 };
 
+export const getCurrentUser = (req: Request, res: Response) => {};
+
 export const logOutController = (_: Request, res: Response) => {
   res.clearCookie("accessToken");
   res.status(201).json({
